3d29448d0303209721491340e971fc20d86490cf
Pranathi Tupakula <ptupakula3@gatech.edu>
2016-04-23 17:43:46 -0400

    Added initial php

--- gttrain/src/AppBundle/Controller/SearchTrainController.php
+++ gttrain/src/AppBundle/Controller/SearchTrainController.php
@@ -1,26 +1,40 @@
 <?php
-// src/AppBundle/Controller/LoginController.php
+// src/AppBundle/Controller/LoginController.php]
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use \mysqli;
 
 class SearchTrainController extends Controller
 {
+	
+
+	
     /**
      * @Route("/searchTrain")
      */
-    public function numberAction()
+    public function show()
     {
+		$stations = $this->db_getStations();
         $html = $this->container->get('templating')->render(
             'searchTrain.html.twig',
-            array('luckyNumberList' => 1)
+            array("stations" => $stations)
         );
 
         return new Response($html);
     }
+	
+	private function db_getStations() {
+		$db = new mysqli("emptystream.com", "cs4400_test", "happy stuff", "cs4400_test");
+		$result = $db->query("SELECT * FROM Station");
+		return $result->fetch_all();
+	}
+	
+
 }
+
 
 ?>
 
