5dec55c3a412ac93fc6de11cb4430292cd816631
Devanathan, Jayanth <jdevanathan3@gatech.edu>
2016-04-23 19:59:58 -0400

    Added student discount functionality.

--- gttrain/src/AppBundle/Controller/AddSchoolInfoController.php
+++ gttrain/src/AppBundle/Controller/AddSchoolInfoController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
 
 class AddSchoolInfoController extends Controller
 {
@@ -13,6 +15,29 @@ class AddSchoolInfoController extends Controller
      */
     public function numberAction()
     {
+        $html = $this->container->get('templating')->render(
+            'addSchoolInfo.html.twig',
+            array('luckyNumberList' => 1)
+        );
+
+        return new Response($html);
+    }
+
+    /**
+     * @Route("/getSchoolInfo"), name = "getSchoolInfo"
+     * @Method({"POST"})
+   */
+    public function getSchoolInfo(Request $request) {
+        $email = $request->request->get('schoolEmailID');
+		
+	//$html = $this->container->get('templating')->render('selectDeparture.html.twig');
+	//return new Response($html);
+        //$args = array(["departStation" => $departStation, "arriveStation" => $arriveStation, "date" => $date]);
+        //return $this->redirectToRoute('selectDeparturePost', $args, 301);
+	$isValid = filter_var($email, FILTER_VALIDATE_EMAIL); 
+
+        $isValid = preg_match("/(\S)+.(edu)/", $email, $matches);
+	var_dump($isValid);
         $html = $this->container->get('templating')->render(
             'addSchoolInfo.html.twig',
             array('luckyNumberList' => 1)
--- gttrain/src/AppBundle/Controller/SearchTrainController.php
+++ gttrain/src/AppBundle/Controller/SearchTrainController.php
@@ -5,6 +5,8 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
 use \mysqli;
 
 class SearchTrainController extends Controller
@@ -14,6 +16,7 @@ class SearchTrainController extends Controller
 	
     /**
      * @Route("/searchTrain")
+     * @Method({"GET"})
      */
     public function show()
     {
@@ -26,6 +29,21 @@ class SearchTrainController extends Controller
 
         return new Response($html);
     }
+    /**
+     * @Route("/searchTrain")
+     * @Method({"POST"})
+     *
+     */
+	public function processPost(Request $request) {
+        $departStation = $request->request->get('departStation');
+        $arriveStation = $request->request->get('arriveStation');
+        $date = $request->request->get('reservationDate');
+		
+		//$html = $this->container->get('templating')->render('selectDeparture.html.twig');
+		//return new Response($html);
+	        $args = array(["departStation" => $departStation, "arriveStation" => $arriveStation, "date" => $date]);
+	        return $this->redirectToRoute('selectDeparturePost', $args, 301);
+        }
 	
 	private function db_getStations() {
 		$db = new mysqli("emptystream.com", "cs4400_test", "happy stuff", "cs4400_test");
@@ -33,14 +51,6 @@ class SearchTrainController extends Controller
 		return $result->fetch_all();
 	}
 	
-	public function processPost(Request $request) {
-        $departStation = $request->request->get('departStation');
-        $arriveStation = $request->request->get('arriveStation');
-        $date = $request->request->get('reservationDate');
-		
-		$html = $this->container->get('templating')->render('selectDeparture.html.twig');
-		return new Response($html);
-    }
 
 	
 	
