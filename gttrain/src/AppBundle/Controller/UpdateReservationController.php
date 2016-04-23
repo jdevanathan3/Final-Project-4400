019d8fbaa54b6fa1454efa21995036a98c8fba0a
Devanathan, Jayanth <jdevanathan3@gatech.edu>
2016-04-23 16:26:57 -0400

    Additional html changes.

--- gttrain/src/AppBundle/Controller/UpdateReservationController.php
+++ gttrain/src/AppBundle/Controller/UpdateReservationController.php
@@ -9,7 +9,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class UpdateReservationController extends Controller
 {
     /**
-     * @Route("/reservation")
+     * @Route("/reservation/search")
      */
     public function show()
     {
@@ -21,8 +21,28 @@ class UpdateReservationController extends Controller
         return new Response($html);
     }
 
+    /**
+     *@Route("/reservation/select")
+     */
     public function goToSelectReservation() {
-        
+        $html = $this->container->get('templating')->render(
+            'updateReservation_select.html.twig',
+            array('luckyNumberList' => 1)
+        );
+
+        return new Response($html);
+    }
+    
+    /**
+     * @Route("/reservation/update")
+     */
+    public function goToUpdateReservation() {
+        $html = $this->container->get('templating')->render(
+            'updateReservation_update.html.twig',
+            array('luckyNumberList' => 1)
+        );
+
+        return new Response($html);
     }
 }
 
