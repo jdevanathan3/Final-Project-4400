2ccd0acb227c54f95a1232355c87642debd2633e
Patrick Tam <pjztam@gmail.com>
2016-04-24 16:49:34 -0400

    updated update reservation controler to remove uneeded files

--- gttrain/src/AppBundle/Controller/reservation/UpdateReservationController.php
+++ gttrain/src/AppBundle/Controller/reservation/UpdateReservationController.php
@@ -8,15 +8,6 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class UpdateReservationController extends Controller
 {
-    /**
-     * @Route("/reservation/search")
-     */
-    public function show()
-    {
-        $html = $this->container->get('templating')->render('reservation/updateReservation_search.html.twig');
-        return new Response($html);
-    }
-
     private function db_getReservation($reservationID) {
         $db = new mysqli("emptystream.com", "cs4400_test", "happy stuff", "cs4400_test");
         $result = $db->query("SELECT * FROM Station");
