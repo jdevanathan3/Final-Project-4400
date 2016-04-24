4aea304b4d86e1776808aee92ccee8f4fbf968fb
Pranathi Tupakula <ptupakula3@gatech.edu>
2016-04-24 01:35:25 -0400

    Added view review inital functionality

---
+++ gttrain/src/AppBundle/Controller/ViewReviewSearchController.php
@@ -0,0 +1,38 @@
+<?php
+// src/AppBundle/Controller/LoginController.php
+namespace AppBundle\Controller;
+
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class ViewReviewSearchController extends Controller
+{
+    /**
+     * @Route("/viewReviewSearch")
+     */
+    public function show()
+    {
+        $html = $this->container->get('templating')->render(
+            'viewReviewSearch.html.twig',
+            array('luckyNumberList' => 1)
+        );
+
+        return new Response($html);
+    }
+	
+	 /**
+     * @Route("/viewReviewSearch")
+     * @Method({"POST"})
+     *
+     */
+	public function processPost(Request $request) {
+        $trainNumber = $request->request->get('trainNumberReviewSearch');
+        
+        return $this->redirectToRoute('viewReview', 
+            ["trainNumber" => trainNumber],
+            302);
+	}
+}
+
+?>
