03f6101089f2a00798732fefcfb09695cf936121
Pranathi Tupakula <ptupakula3@gatech.edu>
2016-04-24 14:55:35 -0400

    view review fully functional

--- gttrain/src/AppBundle/Controller/ViewReviewController.php
+++ gttrain/src/AppBundle/Controller/ViewReviewController.php
@@ -3,23 +3,47 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use \mysqli;
+
+use Symfony\Component\Security\Core\Authentication\Token\UsernamePasswordToken;
+use Symfony\Component\Security\Http\Event\InteractiveLoginEvent;
 
 class ViewReviewController extends Controller
 {
     /**
-     * @Route("/viewReview")
+     * @Route("/viewReview", name="viewReview")
      */
-    public function show()
+    public function show(Request $request)
     {
-        $html = $this->container->get('templating')->render(
+		$inputs = array(
+            "trainNumber" => $request->query->get('trainNumber')
+        );
+		$reviews = $this->db_getReviews($inputs['trainNumber']);
+		$html = $this->container->get('templating')->render(
             'viewReview.html.twig',
-            array('luckyNumberList' => 1)
+            array("reviews" => $reviews)
         );
 
+		
         return new Response($html);
     }
+	
+	private function db_getReviews($trainNumber) {
+		$db = new mysqli("emptystream.com", "cs4400_test", "happy stuff", "cs4400_test");
+        $reviews = $db->query("SELECT Review.Rating, Review.Comment FROM Review WHERE Train_Number = '" . $trainNumber . "' ");
+		
+		$allReviews = array();
+		for ($x = 0; $x < $reviews->num_rows; $x++)
+        {
+            $allReviews[$x] = $reviews->fetch_assoc();
+        }
+		return $allReviews;
+	}
+	
 }
 
 ?>
