0766a4c05b719e0e11504754a76cf6f5ebeced12
Patrick Tam <pjztam@gmail.com>
2016-04-24 01:45:50 -0400

    fixed review search

--- gttrain/src/AppBundle/Controller/ViewReviewSearchController.php
+++ gttrain/src/AppBundle/Controller/ViewReviewSearchController.php
@@ -3,6 +3,7 @@
 namespace AppBundle\Controller;
 
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Method;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
